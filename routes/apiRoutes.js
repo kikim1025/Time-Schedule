6b9f3c0c56e58facec33c23a88fc68268f7b8fb4
Ki <kikim1025@gmail.com>
2019-03-18 23:54:36 -0400

    html route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,13 @@
 // Simple array to keep track of appointments in object format
 const appointments = [{ name: 'Ki', day: 'Monday', hour: '11am' }];
 
+// Define weekdays to be shown on schedule, as well as hour heading
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+// Define specific hours to be shown per day
+const DAY_HOURS = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+
+
 // check if slot is already occupied, which can happen if another user submits appointment post data retrieval by user 
 function checkDupAppointments(aList, a2) {
     for (let a1 of aList) {
