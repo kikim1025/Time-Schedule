fc3ee59e16eb3d45489c7abc3c72bf4beab29141
Ki <kikim1025@gmail.com>
2019-03-19 08:46:30 -0400

    heroku

---
+++ client/build/precache-manifest.ef3d0669188df2b64d8ea77d1f5499ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7b07d20164f6b43b81a0",
+    "url": "/static/js/main.fae422dc.chunk.js"
+  },
+  {
+    "revision": "ca7f6a21f33bd1722b17",
+    "url": "/static/js/2.b7987909.chunk.js"
+  },
+  {
+    "revision": "7b07d20164f6b43b81a0",
+    "url": "/static/css/main.7b1979e1.chunk.css"
+  },
+  {
+    "revision": "ca7f6a21f33bd1722b17",
+    "url": "/static/css/2.e2234688.chunk.css"
+  },
+  {
+    "revision": "4bc65750d183f906ae2baf60ade4e0d5",
+    "url": "/index.html"
+  }
+];
